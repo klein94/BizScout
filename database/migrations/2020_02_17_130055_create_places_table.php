a5eeaae022c92198e2ef498611721178d76ef090
clienn <dramirez.jbsa@gmail.com>
2020-02-28 22:55:45 +0800

    db places

---
+++ database/migrations/2020_02_17_130055_create_places_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->text('address')->nullable();
+            $table->string('contact')->nullable();
+            $table->text('photo')->nullable();
+
+            # search category
+            $table->string('type')->nullable();
+            $table->string('keyword')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
